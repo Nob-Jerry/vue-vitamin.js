17213a94cbf0c8af95977d1e5b295cd412544dd4
Nob-Jerry <trantuan88261@gmail.com>
2025-02-16 10:23:40 +0700

    Commit01-UpdateStruture

---
+++ src/components/Header.vue
@@ -0,0 +1,4 @@
+<template>
+  <router-link to="/home"><button class="btn btn-primary me-2">Home</button></router-link>
+  <router-link to="/about"><button class="btn btn-primary">About</button></router-link>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import MainLayout from "@/layouts/MainLayout.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/home",
+      name: "MainLayout",
+      component: MainLayout,
+      children: [
+        {
+          path: "/home",
+          name: "Home",
+          component: Home,
+        },
+        {
+          path: "/about",
+          name: "About",
+          component: About,
+        },
+      ],
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+  ],
+});
+
+export default router;
